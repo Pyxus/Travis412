cbbb9b3b2a0dbdc66d387b4d45bf773c935b3c85
Ezekiel Halley <eze.halley@gmail.com>
2021-12-17 15:32:53 -0500

    update

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,16 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
+	
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(10.0, 5.0), 2.0, .001);
+	}
+	
+	@Test 
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3), 15, .001);
+	}
 }
