9daaa4e0807ac81baf69c2c0ddfca2f261af6a62
Ezekiel Halley <eze.halley@gmail.com>
2021-12-17 15:34:57 -0500

    update

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -8,4 +8,12 @@ public class SimpleCalculator {
 	public int minus(int a, int b) {
 		return a - b;
 	}
+	
+	public double divide(double a, double b) {
+		return a / b;
+	}
+	
+	public double multiply(double a, double b) {
+		return a * b;
+	}
 }
